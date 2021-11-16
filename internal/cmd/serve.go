5a79115a67296edbdbd5c4352beec1483dee324a
Johannes Donath <johannesd@torchmind.com>
2021-11-16 03:20:30 +0100

    Added an option to anonymize remote addresses.

--- internal/cmd/serve.go
+++ internal/cmd/serve.go
@@ -9,16 +9,24 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/google/subcommands"
 	template "github.com/hashicorp/go-sockaddr/template"
+	"log"
+	"net"
 	"net/http"
 	"os"
+	"runtime"
+	"strings"
 	"time"
 )
 
+const fullyAnonymizedAddressV4 = "*.*.*.*"
+const fullyAnonymizedAddressV6 = "*:*:*:*:*:*"
+
 type serveCommand struct {
-	bind      string
-	context   string
-	generator string
-	timeout   time.Duration
+	bind                string
+	context             string
+	generator           string
+	anonymizeRemoteAddr bool
+	timeout             time.Duration
 }
 
 func ServeCommand() subcommands.Command {
@@ -82,6 +90,8 @@ func (cmd *serveCommand) SetFlags(f *flag.FlagSet) {
 	f.StringVar(&cmd.bind, "bind", "0.0.0.0:8080", "selects the network address to bind to")
 	f.StringVar(&cmd.context, "ctx", "", "selects the context path")
 	f.StringVar(&cmd.generator, "generator", defaultGenerator, "selects the default generator")
+
+	f.BoolVar(&cmd.anonymizeRemoteAddr, "anonymize-remote-addr", false, "anonymizes remote addresses within application logs")
 	f.DurationVar(&cmd.timeout, "timeout", 15*time.Second, "selects the response timeout")
 }
 
@@ -101,7 +111,11 @@ func (cmd *serveCommand) Execute(context.Context, *flag.FlagSet, ...interface{})
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
+	if cmd.anonymizeRemoteAddr {
+		r.Use(anonymizeLogger)
+	} else {
+		r.Use(middleware.Logger)
+	}
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(cmd.timeout))
 
@@ -142,4 +156,64 @@ func exposeParams(next http.Handler) http.Handler {
 
 		next.ServeHTTP(res, req.WithContext(ctx))
 	})
+}
+
+func anonymizeLogger(next http.Handler) http.Handler {
+	color := true
+	if runtime.GOOS == "windows" {
+		color = false
+	}
+
+	f := &middleware.DefaultLogFormatter{
+		Logger:  log.New(os.Stdout, "", log.LstdFlags),
+		NoColor: !color,
+	}
+
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		anonymousReq := *req
+		anonymousReq.RemoteAddr = anonymizeRemoteAddress(req.RemoteAddr)
+
+		entry := f.NewLogEntry(&anonymousReq)
+		ww := middleware.NewWrapResponseWriter(res, req.ProtoMajor)
+
+		t1 := time.Now()
+		defer func() {
+			entry.Write(ww.Status(), ww.BytesWritten(), ww.Header(), time.Since(t1), nil)
+		}()
+
+		next.ServeHTTP(ww, middleware.WithLogEntry(req, entry))
+	})
+}
+
+func anonymizeRemoteAddress(remoteAddr string) string {
+	hostname, _, err := net.SplitHostPort(remoteAddr)
+	if err != nil {
+		return fullyAnonymizedAddressV4
+	}
+
+	ip := net.ParseIP(hostname)
+	if ip == nil {
+		return fullyAnonymizedAddressV4
+	}
+
+	if ipv4 := ip.To4(); ipv4 != nil {
+		enc := strings.Split(ipv4.String(), ".")
+		if len(enc) != 4 {
+			return fullyAnonymizedAddressV4
+		}
+
+		return enc[0] + "." + enc[1] + ".*.*"
+	}
+
+	ipv6 := ip.To16()
+	if ipv6 == nil {
+		return fullyAnonymizedAddressV4
+	}
+
+	enc := strings.Split(ipv6.String(), ":")
+	if len(enc) < 4 {
+		return fullyAnonymizedAddressV6
+	}
+
+	return enc[0] + ":" + enc[1] + ":" + enc[2] + ":" + enc[3] + "::*"
 }
